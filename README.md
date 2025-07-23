e5d91b2a96b46a428e92090d6a202cbeb53a77e9
hexterminal <hellojonesartz@gmail.com>
2025-07-23 23:04:11 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,34 +1,25 @@
 # Python RSS Reader
-
-The Python RSS Reader is a powerful and versatile tool designed to fetch and parse RSS feeds, providing users with a streamlined way to stay updated with the latest news and content from their favorite websites. This project leverages Python’s robust libraries to deliver a seamless experience in aggregating and displaying RSS feed data.
+This RSS reader is a simple reader that fetchs the XML data and then outputs the information on the console.
 
 ## Key Features
-
-Fetch and Parse RSS Feeds: Utilize Python's requests and xml.etree.ElementTree libraries to fetch and parse RSS feeds from various sources.
-
-User-Friendly Output: Display feed data, such as titles, links, publication dates, descriptions, and media content, in a clear and readable format.
-
-Enhanced Readability: Integrate colorama to highlight important information like titles, making it easier to scan through feed items.
-
-Namespace Handling: Properly handle namespaces to accurately extract media-specific elements and other namespaced data.
-
-Extensibility: Easily extend the reader to include additional RSS feed sources and customize the output format to suit specific needs.
+- Improved readability
+- Check for invalid URL
+- Easy to use
 
 
 ## Install 
-
-```bash 
-  git clone https://github.com/erkankavas/python-rss-reader.git
+```bash
+  git clone https://github.com/evenblad3/python-rss-reader.git
   cd python-rss-reader
-  pip install requests colorama
+  pip install rich
+  pip install requests
   ```
 
 ## How to Run?
+```bash
+python main.py
+```
+It then asks for the URL you want to read, paste the url and you're good to go.
 
-The sample code below uses rss_feed_url set to a popular Turkish news website. If you want to use a different RSS feed, please edit the rss_feed_url variable in main.py accordingly.
-
-## Support
-
-https://x.com/erkankavas
-
-https://www.linkedin.com/in/erkankavas/
+---
+Original author: [erkankavas](https://github.com/erkankavas/python-rss-reader)
--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import requests
 import xml.etree.ElementTree as ET
-from colorama import Fore, Back, Style
+from rich.console import Console
 
-print(Fore.RED + 'loading...')
+console = Console()
 
-rss_feed_url = "https://rss.haberler.com/rss.asp?kategori=sondakika"
+while True:
+    rss_feed_url = input("Enter the URL of the feed, q to quit: ")
+    if rss_feed_url.startswith("http://") or rss_feed_url.startswith("https://"):
+        break
+    elif rss_feed_url.lower().startswith("q") or rss_feed_url == "exit":
+        exit()
+    else:
+        console.print("[bold red]Invalid URL!\n[/bold red]")
+        continue
+    # rss_feed_url = "https://msrc.microsoft.com/blog/categories/microsoft-threat-hunting/feed"
 
-response = requests.get(rss_feed_url)
-rss_feed = response.content
+console.print("[green]\nLoading RSS...[/green]\n")
 
-root = ET.fromstring(rss_feed)
+try:
+    response = requests.get(rss_feed_url)
+    if response.status_code == requests.codes.ok:
+        root = ET.fromstring(response.content)
+        
+        def extract_text(element):
+            if element is not None:
+                return element.text
+            return None
+        
+        items = root.findall('.//item')
+        for item in items:
+            title = extract_text(item.find('title'))
+            link = extract_text(item.find('link'))
+            pubDate = extract_text(item.find('pubDate'))
+            description = extract_text(item.find('description'))
+            console.print(f"[bold white]{title}[/bold white]")
+            console.print(f"[underline blue]{link}[/underline blue]")
+            console.print(f"[green]{pubDate}[/green]")
+            print(f"{description}")
+            print()
+        
+        
+        console.print("\n[green]End of the feed.[/green]\n")
+    else:
+        console.print("[bold red]Invalid URL![/bold red]")
 
-def extract_text(element):
-    if element is not None:
-        return element.text
-    return None
-
-items = root.findall('.//item')
-for item in items:
-    title = extract_text(item.find('title'))
-    link = extract_text(item.find('link'))
-    description = extract_text(item.find('description'))
-    
-    print(Fore.RED + f"{title}")
-    print(Fore.YELLOW + f"-> {link}")
-    print(Style.RESET_ALL)
-    print(f"{description}")
-
-    print("\n")
+except Exception as e:
+    print(e)
